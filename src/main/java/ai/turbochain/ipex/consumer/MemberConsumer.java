a486a3b4eedc7658c3bffb7d72477b45d9bd39f0
zhangmingcheng <zhangmingcheng@pansoft.com>
2019-07-22 10:12:58 +0800

    修改账户名生成规则，使合约与链币同一个账户

--- src/main/java/ai/turbochain/ipex/consumer/MemberConsumer.java
+++ src/main/java/ai/turbochain/ipex/consumer/MemberConsumer.java
@@ -42,87 +42,89 @@ import ai.turbochain.ipex.util.MessageResult;
 
 @Component
 public class MemberConsumer {
-    private Logger logger = LoggerFactory.getLogger(MemberConsumer.class);
-    @Autowired
-    private RestTemplate restTemplate;
-    @Autowired
-    private CoinService coinService;
-    @Autowired
-    private MemberWalletService memberWalletService;
-    @Autowired
-    private MemberLegalCurrencyWalletService memberLegalCurrencyWalletService;
-    @Autowired
-    private RewardActivitySettingService rewardActivitySettingService;
-    @Autowired
-    private MemberService memberService;
-    @Autowired
-    private RewardRecordService rewardRecordService;
-    @Autowired
-    private MemberTransactionService memberTransactionService;
-    @Autowired
-    private ESUtils esUtils ;
+	private Logger logger = LoggerFactory.getLogger(MemberConsumer.class);
+	@Autowired
+	private RestTemplate restTemplate;
+	@Autowired
+	private CoinService coinService;
+	@Autowired
+	private MemberWalletService memberWalletService;
+	@Autowired
+	private MemberLegalCurrencyWalletService memberLegalCurrencyWalletService;
+	@Autowired
+	private RewardActivitySettingService rewardActivitySettingService;
+	@Autowired
+	private MemberService memberService;
+	@Autowired
+	private RewardRecordService rewardRecordService;
+	@Autowired
+	private MemberTransactionService memberTransactionService;
+	@Autowired
+	private ESUtils esUtils;
 
-    /**
-     * 重置用户钱包地址
-     * @param record
-     */
-    @KafkaListener(topics = {"reset-member-address"})
-    public void resetAddress(ConsumerRecord<String,String> record){
-        String content = record.value();
-        JSONObject json = JSON.parseObject(content);
-        Coin coin = coinService.findByUnit(record.key());
-        Assert.notNull(coin,"coin null");
-        if(coin.getEnableRpc()==BooleanEnum.IS_TRUE){
-            MemberWallet memberWallet = memberWalletService.findByCoinUnitAndMemberId(record.key(),json.getLong("uid"));
-            Assert.notNull(memberWallet,"wallet null");
-            String account = "U" + json.getLong("uid")+ GeneratorUtil.getNonceString(4);
-            //远程RPC服务URL,后缀为币种单位
-            String serviceName = "SERVICE-RPC-" + coin.getUnit();
-            try{
-                String url = "http://" + serviceName + "/rpc/address/{account}";
-                ResponseEntity<MessageResult> result = restTemplate.getForEntity(url, MessageResult.class, account);
-                logger.info("remote call:service={},result={}", serviceName, result);
-                if (result.getStatusCode().value() == 200) {
-                    MessageResult mr = result.getBody();
-                    if (mr.getCode() == 0) {
-                        String address =  mr.getData().toString();
-                        memberWallet.setAddress(address);
-                    }
-                }
-            }
-            catch (Exception e){
-                logger.error("call {} failed,error={}",serviceName,e.getMessage());
-            }
-            memberWalletService.save(memberWallet);
-        }
+	/**
+	 * 重置用户钱包地址
+	 * 
+	 * @param record
+	 */
+	@KafkaListener(topics = { "reset-member-address" })
+	public void resetAddress(ConsumerRecord<String, String> record) {
+		String content = record.value();
+		JSONObject json = JSON.parseObject(content);
+		Coin coin = coinService.findByUnit(record.key());
+		Assert.notNull(coin, "coin null");
+		if (coin.getEnableRpc() == BooleanEnum.IS_TRUE) {
+			MemberWallet memberWallet = memberWalletService.findByCoinUnitAndMemberId(record.key(),
+					json.getLong("uid"));
+			Assert.notNull(memberWallet, "wallet null");
+			// String account = "U" + json.getLong("uid")+ GeneratorUtil.getNonceString(4);
+			String account = "U" + json.getLong("uid");
+			// 远程RPC服务URL,后缀为币种单位
+			String serviceName = "SERVICE-RPC-" + coin.getUnit();
+			try {
+				String url = "http://" + serviceName + "/rpc/address/{account}";
+				ResponseEntity<MessageResult> result = restTemplate.getForEntity(url, MessageResult.class, account);
+				logger.info("remote call:service={},result={}", serviceName, result);
+				if (result.getStatusCode().value() == 200) {
+					MessageResult mr = result.getBody();
+					if (mr.getCode() == 0) {
+						String address = mr.getData().toString();
+						memberWallet.setAddress(address);
+					}
+				}
+			} catch (Exception e) {
+				logger.error("call {} failed,error={}", serviceName, e.getMessage());
+			}
+			memberWalletService.save(memberWallet);
+		}
 
-    }
+	}
 
-
-    /**
-     * 	客户注册消息
-     * @param content
-     */
-    @KafkaListener(topics = {"member-register"})
-    public void handle(String content) {
-        logger.info("handle member-register,data={}", content);
-        if (StringUtils.isEmpty(content)) {
-            return;
-        }
-        JSONObject json = JSON.parseObject(content);
-        if(json == null) {
-            return ;
-        }
-        //获取所有支持的币种
-        List<Coin> coins =  coinService.findAll();
-        for(Coin coin:coins) {
-            logger.info("memberId:{},unit:{}",json.getLong("uid"),coin.getUnit());
-            MemberWallet wallet = new MemberWallet();
-            wallet.setCoin(coin);
-            wallet.setMemberId(json.getLong("uid"));
-            wallet.setBalance(new BigDecimal(0));
-            wallet.setFrozenBalance(new BigDecimal(0));
-            wallet.setAddress("");
+	/**
+	 * 客户注册消息
+	 * 
+	 * @param content
+	 */
+	@KafkaListener(topics = { "member-register" })
+	public void handle(String content) {
+		logger.info("handle member-register,data={}", content);
+		if (StringUtils.isEmpty(content)) {
+			return;
+		}
+		JSONObject json = JSON.parseObject(content);
+		if (json == null) {
+			return;
+		}
+		// 获取所有支持的币种
+		List<Coin> coins = coinService.findAll();
+		for (Coin coin : coins) {
+			logger.info("memberId:{},unit:{}", json.getLong("uid"), coin.getUnit());
+			MemberWallet wallet = new MemberWallet();
+			wallet.setCoin(coin);
+			wallet.setMemberId(json.getLong("uid"));
+			wallet.setBalance(new BigDecimal(0));
+			wallet.setFrozenBalance(new BigDecimal(0));
+			wallet.setAddress("");
 //            if(coin.getEnableRpc() == BooleanEnum.IS_TRUE) {
 //                String account = "U" + json.getLong("uid");
 //                //远程RPC服务URL,后缀为币种单位
@@ -149,45 +151,48 @@ public class MemberConsumer {
 //            else{
 //                wallet.setAddress("");
 //            }
-            //保存
-            memberWalletService.save(wallet);
-            
-           
-            MemberLegalCurrencyWallet memberLegalCurrencyWallet = new MemberLegalCurrencyWallet();
-            
-            memberLegalCurrencyWallet.setCoin(coin);
-            memberLegalCurrencyWallet.setMemberId(json.getLong("uid"));
-            memberLegalCurrencyWallet.setBalance(new BigDecimal(0));
-            memberLegalCurrencyWallet.setFrozenBalance(new BigDecimal(0));
-            
-            memberLegalCurrencyWalletService.save(memberLegalCurrencyWallet);
-        }
-        //注册活动奖励
-        RewardActivitySetting rewardActivitySetting = rewardActivitySettingService.findByType(ActivityRewardType.REGISTER);
-        if (rewardActivitySetting!=null){
-            MemberWallet memberWallet=memberWalletService.findByCoinAndMemberId(rewardActivitySetting.getCoin(),json.getLong("uid"));
-            if (memberWallet==null){return;}
-            BigDecimal amount3=JSONObject.parseObject(rewardActivitySetting.getInfo()).getBigDecimal("amount");
-            memberWallet.setBalance(BigDecimalUtils.add(memberWallet.getBalance(),amount3));
-            memberWalletService.save(memberWallet);
-            Member member = memberService.findOne(json.getLong("uid"));
-            RewardRecord rewardRecord3 = new RewardRecord();
-            rewardRecord3.setAmount(amount3);
-            rewardRecord3.setCoin(rewardActivitySetting.getCoin());
-            rewardRecord3.setMember(member);
-            rewardRecord3.setRemark(rewardActivitySetting.getType().getCnName());
-            rewardRecord3.setType(RewardRecordType.ACTIVITY);
-            rewardRecordService.save(rewardRecord3);
-            MemberTransaction memberTransaction = new MemberTransaction();
-            memberTransaction.setFee(BigDecimal.ZERO);
-            memberTransaction.setAmount(amount3);
-            memberTransaction.setSymbol(rewardActivitySetting.getCoin().getUnit());
-            memberTransaction.setType(TransactionType.ACTIVITY_AWARD);
-            memberTransaction.setMemberId(member.getId());
-            memberTransaction.setDiscountFee("0");
-            memberTransaction.setRealFee("0");
-            memberTransaction = memberTransactionService.save(memberTransaction);
-        }
+			// 保存
+			memberWalletService.save(wallet);
 
-    }
+			MemberLegalCurrencyWallet memberLegalCurrencyWallet = new MemberLegalCurrencyWallet();
+
+			memberLegalCurrencyWallet.setCoin(coin);
+			memberLegalCurrencyWallet.setMemberId(json.getLong("uid"));
+			memberLegalCurrencyWallet.setBalance(new BigDecimal(0));
+			memberLegalCurrencyWallet.setFrozenBalance(new BigDecimal(0));
+
+			memberLegalCurrencyWalletService.save(memberLegalCurrencyWallet);
+		}
+		// 注册活动奖励
+		RewardActivitySetting rewardActivitySetting = rewardActivitySettingService
+				.findByType(ActivityRewardType.REGISTER);
+		if (rewardActivitySetting != null) {
+			MemberWallet memberWallet = memberWalletService.findByCoinAndMemberId(rewardActivitySetting.getCoin(),
+					json.getLong("uid"));
+			if (memberWallet == null) {
+				return;
+			}
+			BigDecimal amount3 = JSONObject.parseObject(rewardActivitySetting.getInfo()).getBigDecimal("amount");
+			memberWallet.setBalance(BigDecimalUtils.add(memberWallet.getBalance(), amount3));
+			memberWalletService.save(memberWallet);
+			Member member = memberService.findOne(json.getLong("uid"));
+			RewardRecord rewardRecord3 = new RewardRecord();
+			rewardRecord3.setAmount(amount3);
+			rewardRecord3.setCoin(rewardActivitySetting.getCoin());
+			rewardRecord3.setMember(member);
+			rewardRecord3.setRemark(rewardActivitySetting.getType().getCnName());
+			rewardRecord3.setType(RewardRecordType.ACTIVITY);
+			rewardRecordService.save(rewardRecord3);
+			MemberTransaction memberTransaction = new MemberTransaction();
+			memberTransaction.setFee(BigDecimal.ZERO);
+			memberTransaction.setAmount(amount3);
+			memberTransaction.setSymbol(rewardActivitySetting.getCoin().getUnit());
+			memberTransaction.setType(TransactionType.ACTIVITY_AWARD);
+			memberTransaction.setMemberId(member.getId());
+			memberTransaction.setDiscountFee("0");
+			memberTransaction.setRealFee("0");
+			memberTransaction = memberTransactionService.save(memberTransaction);
+		}
+
+	}
 }
